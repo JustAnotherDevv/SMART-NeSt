c01c55872c2f1eae58759c08263fedd415694b3f
JustAnotherDevv <daniel_zarzecki@wp.pl>
2023-11-19 10:56:20 +0300

    Excluded build from gitignore

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,65 +1,65 @@
 // @ts-nocheck
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Link from "next/link";
+import { execHaloCmdWeb } from "@arx-research/libhalo/api/web.js";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
-import {execHaloCmdWeb} from "@arx-research/libhalo/api/web.js";
 
 const Home: NextPage = () => {
-  const [statusText, setStatusText] = useState([])
-  const [infoText, setInfoText] = useState([])
-  const [isCommunity, setIsCommunity] = useState(false)
+  const [statusText, setStatusText] = useState([]);
+  const [infoText, setInfoText] = useState([]);
+  const [isCommunity, setIsCommunity] = useState(false);
 
   function extractCharacters(inputString) {
     if (inputString.length < 6) {
-      return 'String is too short';
+      return "String is too short";
     }
-  
+
     const firstThree = inputString.substring(0, 3);
     const lastThree = inputString.substring(inputString.length - 3);
-  
+
     return firstThree + " ... " + lastThree;
   }
 
-    async function btnClick() {
-        const command = {
-            name: "sign",
-            keyNo: 1,
-            message: "010203",
-            /* uncomment the line below if you get an error about setting "command.legacySignCommand = true" */
-            // legacySignCommand: true,
-        };
+  async function btnClick() {
+    const command = {
+      name: "sign",
+      keyNo: 1,
+      message: "010203",
+      /* uncomment the line below if you get an error about setting "command.legacySignCommand = true" */
+      // legacySignCommand: true,
+    };
 
-        let res;
+    let res;
 
-        try {
-            // --- request NFC command execution ---
-            res = await execHaloCmdWeb(command, {
-                statusCallback: (cause) => {
-                    if (cause === "init") {
-                      setInfoText("Please tap the tag to the back of your smartphone and hold it...");
-                    } else if (cause === "retry") {
-                      setInfoText("Something went wrong, please try to tap the tag again...");
-                    } else if (cause === "scanned") {
-                      setInfoText("Tag scanned successfully, post-processing the result...");
-                    } else {
-                      setInfoText(cause);
-                    }
-                }
-            });
-            // the command has succeeded, display the result to the user
-            let tempStatus = statusText;
-            tempStatus.push(res)
-            setStatusText(tempStatus);
-        } catch (e) {
-            // the command has failed, display error to the user
-            // setStatusText('Scanning failed, click on the button again to retry. Details: ' + String(e));
-        }
+    try {
+      // --- request NFC command execution ---
+      res = await execHaloCmdWeb(command, {
+        statusCallback: cause => {
+          if (cause === "init") {
+            setInfoText("Please tap the tag to the back of your smartphone and hold it...");
+          } else if (cause === "retry") {
+            setInfoText("Something went wrong, please try to tap the tag again...");
+          } else if (cause === "scanned") {
+            setInfoText("Tag scanned successfully, post-processing the result...");
+          } else {
+            setInfoText(cause);
+          }
+        },
+      });
+      // the command has succeeded, display the result to the user
+      let tempStatus = statusText;
+      tempStatus.push(res);
+      setStatusText(tempStatus);
+    } catch (e) {
+      // the command has failed, display error to the user
+      // setStatusText('Scanning failed, click on the button again to retry. Details: ' + String(e));
     }
+  }
 
-    async function createCommunity() {
-      setIsCommunity(true)
+  async function createCommunity() {
+    setIsCommunity(true);
   }
 
   return (
@@ -69,31 +69,45 @@ const Home: NextPage = () => {
               {JSON.stringify(statusText[statusText.length - 1].etherAddress, null, 4)}
             
             <ul> */}
-            <div>
-              <h2 className='mx-auto text-center font-extrabold text-2xl'>Network State XYZ</h2>
-              {isCommunity ? <h2 className='mx-auto text-center font-thin text-lg'>Citizens: {statusText.length}</h2> : "" }
-            </div>
-            <ul>
-              {statusText.length != 0 ? (statusText.map((item, index) => (
-                <div className='flex flex-row w-full'>
+      <div>
+        <h2 className="mx-auto text-center font-extrabold text-2xl">Network State XYZ</h2>
+        {isCommunity ? <h2 className="mx-auto text-center font-thin text-lg">Citizens: {statusText.length}</h2> : ""}
+      </div>
+      <ul>
+        {statusText.length != 0
+          ? statusText.map((item, index) => (
+              <div className="flex flex-row w-full">
                 {/* {item.ethereumAddress ? ( */}
-                  <li className='bg-secondary rounded-md px-2 py-3 mx-auto my-6 w-full justify-evenly flex' key={index}><div className='font-bold'>Address: {extractCharacters(item.etherAddress)}</div>  <div>Pub Key: {extractCharacters(item.publicKey)}</div></li>
-                 {/* ) : ""} */} 
-                </div>
-              ))) : ""}
-          </ul>
-    
-            <div className='pb-48 w-full flex mx-auto flex-col px-4 gap-y-4'>
-              {isCommunity ? <button onClick={() => btnClick()} className="btn btn-neutral">Authorize with ERS</button> : "" }
-              {!isCommunity ? (
-                <>
-                  <input type="text" placeholder="Name" className="input input-bordered w-full mt-8" />
-                  <input type="text" placeholder="Description" className="input input-bordered w-full mt-8" />
-                  <button onClick={() => createCommunity()} className="btn btn-neutral">Create Community</button>
-                </>
-              )
-              : "" }
-            </div>
+                <li className="bg-secondary rounded-md px-2 py-3 mx-auto my-6 w-full justify-evenly flex" key={index}>
+                  <div className="font-bold">Address: {extractCharacters(item.etherAddress)}</div>{" "}
+                  <div>Pub Key: {extractCharacters(item.publicKey)}</div>
+                </li>
+                {/* ) : ""} */}
+              </div>
+            ))
+          : ""}
+      </ul>
+
+      <div className="pb-48 w-full flex mx-auto flex-col px-4 gap-y-4">
+        {isCommunity ? (
+          <button onClick={() => btnClick()} className="btn btn-primary">
+            Authorize with ERS
+          </button>
+        ) : (
+          ""
+        )}
+        {!isCommunity ? (
+          <>
+            <input type="text" placeholder="Name" className="input input-bordered w-full mt-8" />
+            <input type="text" placeholder="Description" className="input input-bordered w-full mt-8" />
+            <button onClick={() => createCommunity()} className="btn btn-primary">
+              Create Community
+            </button>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 };
